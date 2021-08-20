318cd0a6751a27e168df8c5245025862c3afa2da
Dominic Reuter <dominic.reuter@fit.fraunhofer.de>
2021-08-20 13:56:47 +0200

    refactor: replaced license headers

--- src/main/java/de/fraunhofer/fit/appstore/controller/ui/UiController.java
+++ src/main/java/de/fraunhofer/fit/appstore/controller/ui/UiController.java
@@ -1,4 +1,20 @@
-package io.dataspaceconnector.controller.gui;
+/*
+ * Copyright 2020 Fraunhofer Institute for Software and Systems Engineering
+ * Copyright 2021 Fraunhofer Institute for Applied Information Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.fraunhofer.fit.appstore.controller.ui;
 
 import de.fraunhofer.fit.appstore.model.search.SearchResult;
 import de.fraunhofer.fit.appstore.services.search.SearchAppService;
@@ -21,11 +37,11 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 
-@CrossOrigin(origins = {"https://drm-appstore.fit.fraunhofer.de", "http://drm-appstore.fit.fraunhofer.de"})
+@CrossOrigin(origins = {"https://localhost", "http://localhost","https://localhost:8080", "http://localhost:8080"})
 @RequiredArgsConstructor
 @RequestMapping("/api/gui")
 @Tag(name="AppStore GUI")
-public class AppGuiController {
+public class UiController {
 
     private final @NonNull AppService appService;
 
